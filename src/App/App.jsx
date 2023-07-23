c50fdc0528ed34a12cef69beea05fb54b009b4ce
georgealv96 <george1996alvarado@gmail.com>
2023-07-23 11:21:08 -0600

    adding new routes and components

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,30 @@
+import Home from '../Home/Home'
+import ByAge from '../ByAge/ByAge'
+import ByCountry from '../ByCountry/ByCountry'
+import ByName from '../ByName/ByName'
 import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  return <></>
+  return (
+    <div>
+      <nav>
+        <Link to="/">HOME</Link>
+        <Link to="/name">BY NAME</Link>
+        <Link to="/country"> BY COUNTRY</Link>
+        <Link to="/age">BY AGE</Link>
+      </nav>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/name" element={<ByName />} />
+          <Route path="/country" element={<ByCountry />} />
+          <Route path="/age" element={<ByAge />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
---
+++ src/ByCountry/ByCountry.jsx
@@ -0,0 +1,3 @@
+export default function ByCountry() {
+  return <></>
+}
